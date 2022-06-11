f8f0a4ef250d4baa113e736b0e6a601547652636
KseniaMusagitova <kseniamusagitova@gmail.com>
2022-06-11 18:50:57 -0500

    Return the index of the first occurrence

---
+++ Implement_strStr().py
@@ -0,0 +1,23 @@
+
+
+class Solution(object):
+
+    def strStr(self, haystack, needle):
+
+        global index
+        index = haystack.find(needle)
+        return index
+
+sol = Solution()
+print(sol.strStr("hello", ""))
+
+
+
+
+
+
+
+
+
+
+
